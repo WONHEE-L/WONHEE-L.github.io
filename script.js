738002313ad0a46cb410cff2707ddfb3a12bfcbf
WONHEELEE <88381845+WONHEE-L@users.noreply.github.com>
2022-06-16 08:21:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-(function () {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    
-    for (var i = 0; i < names.length; i++) {
-         console.log("Hello " + names[i]);
-    }
+(function (window) {
 
-       var firstLetter = names[i].charAt(0).toLowerCase();
+      var names = ["Yaakov","John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]; 
+      var name = ["Yaakov"];
+          console.log("Hello " + name)
 
-       if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
-      } else {
-        helloSpeaker.speak(names[i]);
-      }
+      var byeSpeakers = ["John", "Jen", "Jason"]      
+        for (var i = 0; i < byeSpeakers.length; i++) {
+            console.log("Good bye " + byeSpeakers[i])
+        }
+      var helloSpeakers = ["Paul", "Frank", "Larry", "Paula", "Laura"];
+        for (var i = 0; i < helloSpeakers.length; i++) {
+            console.log("Hello " + helloSpeakers[i]);
+        }
+      var byeSpeaker = ["Jim"];
+         console.log("Good Bye " + byeSpeaker)
 
-    })();
+       //var firstLetter = names[i].charAt(1).toUpperCase();
+       //if (firstLetter === 'J') {
+        // console.log("Good Bye " + names[i]);
+      // } else {
+        // console.log("Hello " + names[i]);
+     // }
+   
+      window.names = names;
+
+   })(window);
        
