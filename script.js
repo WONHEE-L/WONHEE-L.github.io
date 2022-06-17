f49fc6a3f25078a1586ffb7b141843c7811af41a
WONHEELEE <88381845+WONHEE-L@users.noreply.github.com>
2022-06-17 00:24:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
 (function (window) {
 
       var names = ["Yaakov","John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]; 
-      var name = ["Yaakov"];
-          console.log("Hello " + name)
 
-      var byeSpeakers = ["John", "Jen", "Jason"]      
-        for (var i = 0; i < byeSpeakers.length; i++) {
-            console.log("Good bye " + byeSpeakers[i])
-        }
-      var helloSpeakers = ["Paul", "Frank", "Larry", "Paula", "Laura"];
-        for (var i = 0; i < helloSpeakers.length; i++) {
-            console.log("Hello " + helloSpeakers[i]);
-        }
-      var byeSpeaker = ["Jim"];
-         console.log("Good Bye " + byeSpeaker)
-
-       //var firstLetter = names[i].charAt(1).toUpperCase();
-       //if (firstLetter === 'J') {
-        // console.log("Good Bye " + names[i]);
-      // } else {
-        // console.log("Hello " + names[i]);
-     // }
+      for (var i = 0; i < names.length; i++) {   
+       var firstLetter = names[i].charAt(0).toUpperCase();
+       if (firstLetter === 'J') {
+         console.log("Good Bye " + names[i]);
+       } else {
+        console.log("Hello " + names[i]);
+      }
+    }
    
       window.names = names;
 
